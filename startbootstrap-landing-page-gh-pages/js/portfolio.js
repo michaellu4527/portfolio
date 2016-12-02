03a15cb09f6fabd0efa30aedf21af49a4068e423
Michael Lu <mklu2005@yahoo.com>
2016-12-02 10:50:23 -0800

    Added auto scroll and attempted to add scroll spy
    Scroll spy currently doesn't work. Needs to be fixed.

---
+++ startbootstrap-landing-page-gh-pages/js/portfolio.js
@@ -0,0 +1,22 @@
+// Auto scrolls to selected menu item on page
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$(document).ready(function(){
+	$('body').scrollspy({target: '.navbar-fixed-top', offset:50});
+});
+
